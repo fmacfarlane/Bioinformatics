80ca5ab8d9cba1015be715f4de18583b7c186ec8
fmacfarlane <f.macfarlane@dundee.ac.uk>
2014-03-02 14:37:23 +0100

    Python script used to run a ringbuffer on the data in the signal.txt file

--- ringbuffer.py
+++ ringbuffer.py
@@ -1,5 +1,5 @@
 #!usr/bin/python
-
+# set the infile variable to be the signal.txt file
 infile='signal.txt'
 #read in our search sequences
 dataset=open(infile)
@@ -9,8 +9,11 @@ for line in dataset.readlines():
 	for number in line.split():
 		data.append(float(number))
 dataset.close()
+# this reads all of the entries in the signal.txt file as floating numbers and puts them in the array data
 buffersize=60
+# this sets the buffer size, trial and error found that 60 was a suitable buffer size
 buffer=[]
+# creates the buffer array
 buffertotal=0
 position=0
 result=[]
@@ -25,10 +28,11 @@ for v in range(position, len(data)):
 	buffertotal=buffertotal+buffer[v%buffersize]
 	result.append(float(buffertotal)/buffersize)
 print result
-
+# this loop creates the buffer and runs it, organising the data and printing the result
 smooth=open('smoothed.txt', 'w')
+# we open a new text file to hold the buffer results
 for r in result:
 	smooth.write("%s\n" % r)
 smooth.close()
-
+# this send the data from the buffer into a text file which can then be plotted in R
 
